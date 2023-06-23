638f1afd0d8f47807b0d19babdb4c303779e8751
Kembrige <49879155+Kembrige@users.noreply.github.com>
2023-06-23 14:23:29 +0400

    Add files via upload

---
+++ Exz/Program.cs
@@ -0,0 +1,40 @@
+﻿using Exz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+Console.WriteLine("Littleendin");
+
+var list1LE = new List<double>();
+var list2LE = new List<double>();
+
+int n = 60;
+using (BinaryReader reader = new BinaryReader(File.Open("polynomTest2LE.dat", FileMode.Open)))
+{
+    for (int i = 0; i < n; i++)
+    {
+        double num = reader.ReadDouble();
+        if (i < n / 2)
+        {
+            list1LE.Add(num);
+        }
+        else
+        {
+            list2LE.Add(num);
+        }
+    }
+}
+
+Console.WriteLine();
+
+Console.WriteLine("Naive " + Scala.NaiveScalarProduct(list1LE, list2LE));
+Console.WriteLine("Naive Fma  " + Scala.FmaScalarProduct(list1LE, list2LE));
+Console.WriteLine("Ogita Rump Oishi  " + Scala.OgitaRumpOishiDotProduct(list1LE, list2LE));
+
+Console.WriteLine("Errors");
+
+Console.WriteLine("Absolute Error " + Scala.AbsoluteError(Scala.NaiveScalarProduct(list1LE, list2LE), Scala.OgitaRumpOishiDotProduct(list1LE, list2LE)));
+Console.WriteLine("Relative Error " + Scala.RelativeError(Scala.NaiveScalarProduct(list1LE, list2LE), Scala.OgitaRumpOishiDotProduct(list1LE, list2LE)));
+Console.WriteLine("Ulp Error " + Scala.UlpError(Scala.NaiveScalarProduct(list1LE, list2LE), Scala.OgitaRumpOishiDotProduct(list1LE, list2LE)));
---
+++ Exz/Scala.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exz
+{
+    public static class Scala
+    {
+
+        public static (double, double) TwoSum(double a, double b)
+        {
+            var s = a + b;
+            var bs = s - a;
+            var at = s - bs;
+            var t = (b - bs) + (a - at);
+            return (s, t);
+        }
+        public static double NaiveScalarProduct(List<double> x, List<double> y)
+        {
+            var s = 0.0;
+            for (var i = 0; i < x.Count; i++)
+            {
+                s += x[i] * y[i];
+            }
+            
+            return s;
+        }
+
+        public static float NaiveScalarProduct(List<float> x, List<float> y)
+        {
+            var s = 0f;
+            for (var i = 0; i < x.Count; i++)
+            {
+                s += x[i] * y[i];
+            }
+
+            return s;
+        }
+
+        public static double FmaScalarProduct(List<double> x, List<double> y)
+        {
+            var s = 0.0;
+            for (var i = 0; i < x.Count; i++)
+            {
+                s = Math.FusedMultiplyAdd(x[i], y[i], s);
+            }
+
+            return s;
+        }
+
+        public static (double, double) FmaProductTwo(double a, double b)
+        {
+            var p = a * b;
+            var t = Math.FusedMultiplyAdd(a, b, -p);
+            return (p, t);
+        }
+
+        public static double OgitaRumpOishiDotProduct(List<double> x, List<double> y)
+        {
+            var s = 0.0;
+            var c = 0.0;
+            for (int i = 0; i < x.Count; i++)
+            {
+                var (p, pi) = FmaProductTwo(x[i], y[i]);
+                (s, var t) = TwoSum(p, s);
+                c += pi + t;
+            }
+
+            return s + c;
+        }
+        public static double Ulp(double value)
+        {
+            var bits = BitConverter.DoubleToInt64Bits(value);
+            var nextValue = BitConverter.Int64BitsToDouble(bits + 1);
+            return nextValue - value;
+        }
+
+        public static double AbsoluteError(double x, double x_exact)
+        {
+            return Math.Abs(x - x_exact);
+        }
+
+        public static double RelativeError(double x, double x_exact)
+        {
+            return Math.Abs(x - x_exact) / Math.Abs(x_exact) * 100;
+        }
+
+        public static double UlpError(double x, double x_exact)
+        {
+            return Math.Abs(x - x_exact) / Ulp(x_exact);
+        }
+
+        public static string ToBinary(float value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+            string binary = "";
+            for (int i = bytes.Length - 1; i >= 0; i--)
+            {
+                binary += Convert.ToString(bytes[i], 2).PadLeft(8, '0');
+            }
+            return binary;
+        }
+
+        public static float FromBinary(string binaryString)
+        {
+            var intValue = Convert.ToInt32(binaryString, 2);
+            return BitConverter.ToSingle(BitConverter.GetBytes(intValue), 0);
+        }
+
+    }
+}
